885025c5bd9f0b1b5345f5bc3e0c21bc4a77ea31
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-17 09:50:43 -0400

    commit tree f816dc8871c5ff4e4aadbc9022ce50bebc59b973

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    let message = 'This one is on me!';
+    return message
+  }
+  else if (distance > 400 && distance < 2000) {
+    let message = 'That will be twenty bucks.';
+    return message;
+  }
+  else if (distance > 2000 && distance < 2500) {
+    let message = 'I will gladly take your thirty bucks.';
+    return message;
+  }
+  else if (distance >= 2500) {
+    let message = 'No can do.';
+    return message;
+  }
+  console.log(message);
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go."
+  }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(gratuity){
+  let message;
+  switch (gratuity) {
+    case 'generous':
+      message = 'Thank you so much.';
+      return message;
+    case 'not as generous':
+      message = 'Thank you.';
+      return message;
+    case 'thanks for everything':
+      message = 'Bye.';
+      return message;
+      break;
+  }
 }
